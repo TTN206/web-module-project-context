533a13c8ab9f0f03180ee6d55f62ff12df2bfc9e
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-05-27 22:02:46 -0700

    added item functionality, created ProductContext, providing data with ProductContext

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import {ProductContext} from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
-
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			{/* Wrap all of your components/routes in `App.js` inside of `ProductContext.Provider` component. */}
+			{/* the value prop we'll pass in the products state, and an addItem function that will allow us to add books to the cart.*/}
+			<ProductContext.Provider value={{products, addItem}}>
+				<Route exact path="/">
+					{/* we're providing our products state and addItem function we can simplify our products route a bit.*/}
+					<Products />
+				</Route>
+			</ ProductContext.Provider>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
