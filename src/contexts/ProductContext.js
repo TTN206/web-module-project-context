5b0acd3e055cc5771e687370ed3ed667fa8b3ef6
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-05-30 05:55:44 -0700

    created CartContext.js and adding it to app soon

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,6 +1,4 @@
 import {createContext} from 'react'; // import the `createContext` function from the react library and create our `ProductContext`.
 
 export const ProductContext = createContext();
-/*
-we've created our `ProductContext` we can import into our `App.js`. Now we can start providing data across our application!
-*/
+/* created our `ProductContext` we can import into our `App.js`. Now we can start providing data across our application! */
